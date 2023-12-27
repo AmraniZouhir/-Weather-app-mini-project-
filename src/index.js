9ced0b6db8f8db8c98d19806c2e28a9b8ba6688d
Zouhir Amrani <zouhiramrani397@gmail.com>
2023-12-27 16:13:01 +0100

    Add bagrond vidio and text and button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styale.scss'
+import App from './Welcome/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
